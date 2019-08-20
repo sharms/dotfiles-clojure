6004be93881e402fae250e2fc831624e2efa9eff
Steve Harms <sharms@snowfoundry.com>
2019-08-19 18:13:24 -0600

    Add keybindings for Atom

--- configure.sh
+++ configure.sh
@@ -68,3 +68,22 @@ cat << EOF > ~/.clojure/deps.edn
   }
 }
 EOF
+
+mkdir -p ~/.atom
+cat << EOF > ~/.atom/keymap.cson
+'atom-text-editor[data-grammar="source clojure"]':
+  'ctrl-, y':       'chlorine:connect-clojure-socket-repl'
+  'ctrl-, e':       'chlorine:disconnect'
+  'ctrl-, k':       'chlorine:clear-console'
+  'ctrl-, f':       'chlorine:load-file'
+  'ctrl-, b':       'chlorine:evaluate-block'
+  'ctrl-, B':       'chlorine:evaluate-top-block'
+  'ctrl-, i':       'chlorine:inspect-block'
+  'ctrl-, I':       'chlorine:inspect-top-block'
+  'ctrl-, s':       'chlorine:evaluate-selection'
+  'ctrl-, c':       'chlorine:break-evaluation'
+  'ctrl-, C':       'chlorine:source-for-var'
+  'ctrl-, d':       'chlorine:doc-for-var'
+  'ctrl-, x':       'chlorine:run-tests-in-ns'
+  'ctrl-, t':       'chlorine:run-test-for-var'
+EOF
